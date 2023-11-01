b02079a8510d2016b6ba7162aecdeff4ed0ad0f5
Renat Sharapov <renat4418@gmail.com>
2023-11-01 13:32:54 +0300

    change title and vite config, removed global.d.ts

--- src/global.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    VITE_MAPBOX_TOKEN?: string
-    VITE_OPEN_WEATHER_API_KEY?: string
-  }
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'process.env': process.env,
-  },
-})
+export default defineConfig({})
