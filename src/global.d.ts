505ed4ff61bdbbc2f043bfe178681b03b4a7f0e1
Renat Sharapov <renat4418@gmail.com>
2023-11-01 11:29:02 +0300

    add ts

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    VITE_MAPBOX_TOKEN?: string
+    VITE_OPEN_WEATHER_API_KEY?: string
+  }
+}
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env': process.env,
+  },
+})
